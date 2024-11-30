4b2584398d679e7e47bc74800648c8a04d5e9350
shamilka <sham.gurusinghe@gmail.com>
2024-11-30 12:43:51 +0530

    install tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+
+// import {useEffect, useState} from "react";
+
+const App = () => {
+
+  return (
+      <div>
+          {/*<h1>Hello React</h1>*/}
+          <h1 className="text-3xl font-bold underline">
+              Hello React!
+          </h1>
+      </div>
+  )
+}
+
+export default App
+
+// const [myName, setMyName] = useState('Shamilka')
+// const [number, setNumber] = useState(0)
+//
+// useEffect(() => {
+//
+// }, []);
+//
+// return (
+//     <div>
+//         <h1>{number}</h1>
+//         <button onClick={()=>{setNumber(number+1)}}>Increment</button>
+//     </div>
+// )
